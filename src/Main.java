650b13880891d206462f2407153543d2cb7f1571
Jack Ni <a@a>
2024-02-15 19:54:35 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+    public static void main(String[] args) {
+        double salary = 73000;
+        double rent = 1500;
+        double rentInsurance = 80;
+        double internet = 50;
+        double groceries = 250;
+        double dataPlan = 50;
+        double commutingCosts = 300;
+        double savingsAccount = 507.35;
+        int months = 1;
+        while (savingsAccount < 150000) {
+            months ++;
+            if (months % 12 == 0) {
+                salary = salary + salary * (((Math.random() * 3) + 5) /100);
+                rent *= 1 + (((Math.random() * 3) + 3) / 100);
+                rentInsurance *= 1 + (((Math.random() * 3) + 3) / 100);
+                internet *= 1 + (((Math.random() * 3) + 3) / 100);
+                groceries *= 1 + (( (Math.random() * 3) + 3) / 100);
+                dataPlan *= 1 + (( (Math.random() * 3) + 3) / 100);
+                commutingCosts *= 1 + (( (Math.random() * 3) + 3) / 100);
+            }
+            savingsAccount += (salary * 0.45)/12;
+            savingsAccount -= rent;
+            savingsAccount -= rentInsurance;
+            savingsAccount -= internet;
+            savingsAccount -= groceries;
+            savingsAccount -= dataPlan;
+            savingsAccount-= commutingCosts;
+            if ((int) (Math.random() * 5) + 1 == 1) {
+                savingsAccount -= 150;
+            }
+        }
+        System.out.println(months);
+    }
+}
